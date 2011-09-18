668a4b4e569635a14aca62c23f2ef862418db88b
WJ <wookjong.kwak@sv.cmu.edu>
2011-09-17 21:24:17 -0400

    Added Micropost function

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 SampleApp::Application.routes.draw do
+  resources :microposts
+
   get "pages/home"
 
   get "pages/contact"
--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,13 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 0) do
+ActiveRecord::Schema.define(:version => 20110918011754) do
+
+  create_table "microposts", :force => true do |t|
+    t.string   "content"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
