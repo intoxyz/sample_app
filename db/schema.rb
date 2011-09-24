d73725e4baafbddddc4e8d6917e2c3bf91f3f776
WJ <wookjong.kwak@sv.cmu.edu>
2011-09-24 05:17:32 -0400

    Done by 7.3.1

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110924063400) do
+ActiveRecord::Schema.define(:version => 20110924083522) do
 
   create_table "microposts", :force => true do |t|
     t.string   "content"
@@ -32,6 +32,8 @@ ActiveRecord::Schema.define(:version => 20110924063400) do
     t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "encrypted_password"
+    t.string   "salt"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
