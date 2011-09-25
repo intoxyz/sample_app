7fb096dc282ec6388329dd6dd1249fe39cc451fa
WJ <wookjong.kwak@sv.cmu.edu>
2011-09-25 17:13:51 -0400

    modify routes.rb add resources

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ SampleApp::Application.routes.draw do
   resources :users
   resources :sessions, :only => [:new, :create, :destroy]
   resources :microposts, :only => [:create, :destroy]
+  resources :places
 
   match '/signup',  :to => 'users#new'
   match '/signin',  :to => 'sessions#new'
